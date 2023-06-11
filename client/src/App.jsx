63ae32d69a69d8e1e59f4990f271e11f028af724
m-shao <s201083816@ddsbstudent.ca>
2023-06-11 05:04:19 -0400

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,19 @@
-import fullscreen from './images/fullscreen.svg'
-import other from './images/other.svg'
-import send from './images/send.svg'
-import share from './images/share.svg'
-import share2 from './images/share2.svg'
-import volume from './images/volume.svg'
-
-import person1 from './images/image 3.png'
-import person2 from './images/image 4.png'
+import Call from  './components/Call'
+import SetupPage from  './components/SetupPage'
 
 import Navbar from './components/Navbar'
 
-import Webcam from 'react-webcam';
 
+
+// import Sentiment from './components/Sentiment'
 
 function App() {
     return (
         <div className='App bg-neutral-900 text-white w-screen h-screen p-6 flex flex-col gap-6 overflow-hidden'>
+            {/* <Sentiment/> */}
             <Navbar/>
-            <div className='h-full w-full flex gap-4 '>
-                <div className='h-full flex-1 flex flex-col gap-4 relative'>
-                    <div className='h-3/4 bg-blue-500 rounded-md overflow-hidden flex justify-end items-end'>
-                        <img className='object-cover h-full w-full' src={person1} alt="" />
-                        <img className='absolute w-64 rounded-md m-1' src={person2} alt="" />
-                        <Webcam
-                            audio={false}
-                            mirrored={true}
-                            className='w-56 absolute rounded-md m-1'
-                        />
-                    </div>
-                    <div className='h-12 w-full bg-neutral-800 rounded-md flex gap-6 justify-center items-center'>
-                        <img className='h-1/2' src={volume} alt="" />
-                        <img className='h-1/2' src={other} alt="" />
-                        <img className='h-1/2' src={share} alt="" />
-                        <img className='h-1/2' src={share2} alt="" />
-                        <img className='h-1/2' src={fullscreen} alt="" />
-                    </div>
-                </div>
-                <div className='w-1/4 h-full justify-center rounded-md'>
-                    <div className='w-full h-full bg-neutral-800'></div>
-                </div>
-            </div>
+            <Call/>
+            {/* <SetupPage/> */}
         </div>
     )
 }
