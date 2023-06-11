168da499c69e505e6825e1de4131a45f1328afa0
m-shao <s201083816@ddsbstudent.ca>
2023-06-10 23:39:20 -0400

    main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,45 @@
+import fullscreen from './images/fullscreen.svg'
+import other from './images/other.svg'
+import send from './images/send.svg'
+import share from './images/share.svg'
+import share2 from './images/share2.svg'
+import volume from './images/volume.svg'
+
+import person1 from './images/image 3.png'
+import person2 from './images/image 4.png'
+
+import Navbar from './components/Navbar'
+
+import Webcam from 'react-webcam';
+
 
 function App() {
     return (
-        <div className='App text-red-300'>
-            hello there
+        <div className='App bg-neutral-900 text-white w-screen h-screen p-6 flex flex-col gap-6 overflow-hidden'>
+            <Navbar/>
+            <div className='h-full w-full flex gap-4 '>
+                <div className='h-full flex-1 flex flex-col gap-4 relative'>
+                    <div className='h-3/4 bg-blue-500 rounded-md overflow-hidden flex justify-end items-end'>
+                        <img className='object-cover h-full w-full' src={person1} alt="" />
+                        <img className='absolute w-64 rounded-md m-1' src={person2} alt="" />
+                        <Webcam
+                            audio={false}
+                            mirrored={true}
+                            className='w-56 absolute rounded-md m-1'
+                        />
+                    </div>
+                    <div className='h-12 w-full bg-neutral-800 rounded-md flex gap-6 justify-center items-center'>
+                        <img className='h-1/2' src={volume} alt="" />
+                        <img className='h-1/2' src={other} alt="" />
+                        <img className='h-1/2' src={share} alt="" />
+                        <img className='h-1/2' src={share2} alt="" />
+                        <img className='h-1/2' src={fullscreen} alt="" />
+                    </div>
+                </div>
+                <div className='w-1/4 h-full justify-center rounded-md'>
+                    <div className='w-full h-full bg-neutral-800'></div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-fo4n8lfjg77mabd3.us.auth0.com"
+      clientId="GE47Y8e0efmDaESRQdpUUtSnurbao6LB"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
