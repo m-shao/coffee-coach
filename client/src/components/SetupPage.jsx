92f5353bf10048584d898412352a808afb91a396
m-shao <s201083816@ddsbstudent.ca>
2023-06-11 05:40:38 -0400

    text field

--- client/src/components/SetupPage.jsx
+++ client/src/components/SetupPage.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const SetupPage = () => {
   const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    console.log(inputValue)
+    }, [inputValue]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,19 +21,19 @@ const SetupPage = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="max-w-sm">
-        <div className="mb-4">
-          <label htmlFor="inputField" className="block mb-2 font-bold">Input Field</label>
-          <input
-            type="text"
+      <form onSubmit={handleSubmit} className="w-1/2">
+        <div className="mb-4 w-full">
+          <label htmlFor="inputField" className="block mb-2 font-bold">🙌🏼 Tell us about your meeting today 🙌🏼:</label>
+          <textarea
             id="inputField"
             value={inputValue}
             onChange={handleInputChange}
-            className="w-full p-4 border border-gray-300 rounded"
-            placeholder="Enter value"
+            className="w-full p-4 border border-gray-300 text-black rounded"
+            placeholder="Speaker, Interviwee, Topic, Goals, Context, Relationship, Level of Professionalism..."
+            rows={4}
           />
         </div>
-        <button type="submit" className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">
+        <button type="submit" className="w-full px-4 py-2 font-bold text-white bg-neutral-500 rounded hover:bg-neutral-600">
           Submit
         </button>
       </form>
